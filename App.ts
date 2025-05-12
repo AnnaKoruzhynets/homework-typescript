690da6ca31af3f197dd43d18af87a29e23524a44
KoruzhynetsAnna <121615437+KoruzhynetsAnna@users.noreply.github.com>
2025-05-12 18:14:39 +0300

    update code

--- App.ts
+++ App.ts
@@ -13,14 +13,26 @@ let callback = (a: number): number => {
     return 100 + a 
 };
 
+console.log(age);
+console.log(Name);
+console.log(toggle);
+console.log(empty);
+console.log(notInitialize);
+console.log(callback(7));
+
+
 //2
+
 let anything: any = -20;
 
 anything = 'Text';
 
 anything = {};
 
+console.log(anything);
+
 //3
+
 let some: unknown;
 
 some = 'Text';
@@ -31,29 +43,40 @@ if (typeof some === 'string') {
   str = some;
 }
 
+console.log(some);
+
 //4
 
 const person:[string, number]= ['Max', 21];
+
+console.log(person);
+
 //5
 
-enum LoadStatus {
+enum Status {
   LOADING,
-  READY
+  READY,
 }
-
-const pageLoad: LoadStatus = LoadStatus.LOADING;
 
 //6
 
-let unionVar: string | number;
+let union: string | number;
+
+union = "something";
+union = 51;
+
+console.log(union);
 
 //7
 
-let literalVar: 'enable' | 'disable';
+let one: 'enable' | 'disable';
+one = 'enable';
+
+console.log(one);
 
 //8
 
-function showMessage(message: string): void {
+function showMessage(message: string) {
   console.log(message);
 }
 
@@ -65,6 +88,9 @@ function customError(): never {
   throw new Error('Error');
 }
 
+console.log(showMessage("homework"));
+console.log(calc(7, 3));
+console.log(customError);
 //9
 
 type Page = {
@@ -72,12 +98,8 @@ type Page = {
   likes: number;
   accounts: string[];
   status: 'open' | 'close';
-  details?: {
-    createAt: Date;
-    updateAt: Date;
-  }
-}
-
+  details?: {};
+};
 const page1: Page = {
   title: 'The awesome page',
   likes: 100,
@@ -86,12 +108,15 @@ const page1: Page = {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
 const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
+
+console.log(page1);
+console.log(page2);
